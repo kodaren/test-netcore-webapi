3302ce934eb3cc0cbb2b954c471159a0f0d11cfa
Jimmy Karlsson <kodaren@gmail.com>
2019-06-07 21:32:16 +0200

    Added value4

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Rio_Test_WebAPI.Controllers
     [HttpGet]
     public ActionResult<IEnumerable<string>> Get()
     {
-      return new string[] { "value1", "value2", "value3" };
+      return new string[] { "value1", "value2", "value3", "value4" };
     }
 
     // GET api/values/5
